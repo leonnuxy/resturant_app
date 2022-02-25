bc4e20548a1cfd8a923f424dcf82b455722ce1a6
Noel <1leonnoel1@gmail.com>
2022-02-25 00:08:15 -0700

    Web Application

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        
+        <p className="p__opensans">The Fullers set up restaurant in their new home town of Vancouver in 1983. 
+        We offer "dishes inspired by trips around the world."in Canada and United States</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">This is a family owned premium casual dining chain that operates a total of 68 restaurants in Canada and the United States. 
+        Their head office is in Vancouver, British Columbia, Canada.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
