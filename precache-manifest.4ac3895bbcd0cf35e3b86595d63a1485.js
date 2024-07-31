ee02bd53ec2e702f7e189e4e6785da4ef2d46075
Noel <1leonnoel1@gmail.com>
2024-07-30 21:47:56 -0600

    Updates

---
+++ precache-manifest.4ac3895bbcd0cf35e3b86595d63a1485.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f341a6b44640e2b3fe5570d5b8b6db",
+    "url": "/resturant_app/index.html"
+  },
+  {
+    "revision": "e643e11dc4d1bcb5e856",
+    "url": "/resturant_app/static/css/main.6c69e63c.chunk.css"
+  },
+  {
+    "revision": "62c126e22e6550b39018",
+    "url": "/resturant_app/static/js/2.c4347701.chunk.js"
+  },
+  {
+    "revision": "e643e11dc4d1bcb5e856",
+    "url": "/resturant_app/static/js/main.edf803d3.chunk.js"
+  },
+  {
+    "revision": "1a85fed2046828d147a1",
+    "url": "/resturant_app/static/js/runtime~main.f1097793.js"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/resturant_app/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/resturant_app/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/resturant_app/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/resturant_app/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/resturant_app/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/resturant_app/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/resturant_app/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/resturant_app/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/resturant_app/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/resturant_app/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/resturant_app/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/resturant_app/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/resturant_app/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/resturant_app/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/resturant_app/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/resturant_app/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "cc54616224f2b073f9400318c172d8a6",
+    "url": "/resturant_app/static/media/menu.cc546162.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/resturant_app/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/resturant_app/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/resturant_app/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "fd16528f71a845cc74618cf2bd3d8995",
+    "url": "/resturant_app/static/media/welcome.fd16528f.png"
+  }
+]);
